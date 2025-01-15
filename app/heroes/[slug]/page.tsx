83816ad11477cb0cf447641e5b942a18fba5bae9
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-01-15 19:30:15 +0100

    Ajout des images des unités et héros

--- app/heroes/[slug]/page.tsx
+++ app/heroes/[slug]/page.tsx
@@ -1,14 +1,12 @@
-// import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 
-const fetchHero = async ( slug : string ) => {
-    const res = await fetch(`http://localhost:1337/api/heroes/${slug}`);
-    // const resImage = await fetch(`http://localhost:1337/api/upload/files`);
-    const data = await res.json();
-    // const dataImage = await resImage.json();
-    // console.log(dataImage);
-    // const image = dataImage.find((image: { documentId: string }) => image.documentId === data.data.documentId);
-    // data.data.image = image;
-    return data.data;
+const fetchHero = async (slug: string) => {
+  const res = await fetch(
+    `http://localhost:1337/api/heroes/${slug}?populate=*`
+  );
+  const data = await res.json();
+  return data.data;
 };
 
 export default async function Page({ params }: { params: { slug: string } }) {
@@ -17,10 +15,17 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const hero = await fetchHero(slug);
 
   return (
-    <div className="m-20 flex flex-col gap-4">
-        {/* <Image src={`http://localhost:1337/${hero?.image?.url}`} alt={hero?.name} width={500} height={500} /> */}
-      <h1 className="text-4xl">{hero?.name}</h1>
-      <p>{hero?.description}</p>
-    </div>
+    <Card className="m-20 flex gap-4 p-10">
+      <Image
+        src={`http://localhost:1337${hero?.picture?.url}`}
+        alt={hero?.name}
+        width={500}
+        height={500}
+      />
+      <CardContent className="flex flex-col gap-4">
+        <h1 className="text-4xl">{hero?.name}</h1>
+        <p>{hero?.description}</p>
+      </CardContent>
+    </Card>
   );
 }
--- app/unities/[slug]/page.tsx
+++ app/unities/[slug]/page.tsx
@@ -1,7 +1,18 @@
-const fetchUnity = async ( slug : string ) => {
-    const res = await fetch(`http://localhost:1337/api/unities/${slug}`);
-    const data = await res.json();
-    return data.data;
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+const fetchUnity = async (slug: string) => {
+  const res = await fetch(
+    `http://localhost:1337/api/unities/${slug}?populate=*`
+  );
+  const data = await res.json();
+  return data.data;
 };
 
 export default async function Page({ params }: { params: { slug: string } }) {
@@ -10,9 +21,28 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const unity = await fetchUnity(slug);
 
   return (
-    <div className="m-20 flex flex-col gap-4">
-      <h1 className="text-4xl">{unity?.name}</h1>
-      <p>{unity?.description}</p>
+    <div className="m-20 p-10 flex gap-4">
+      <Carousel className="w-full">
+        <CarouselContent>
+          {unity?.picture.map((picture: { url: string }, index: number) => (
+            <CarouselItem key={index}>
+              <Image
+                src={`http://localhost:1337${picture.url}`}
+                alt={unity?.name}
+                width={500}
+                height={500}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl">{unity?.name}</h1>
+        <p>{unity?.description}</p>
+      </div>
     </div>
   );
 }
